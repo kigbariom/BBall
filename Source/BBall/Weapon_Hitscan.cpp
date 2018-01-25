cde8cbaeea599c986efd7b71137b690ae4a75344
Conor Golden <cwgolden@gmail.com>
2018-01-24 22:38:30 -0500

    Adding cone trace logic for hitscan weapon

--- Source/BBall/Weapon_Hitscan.cpp
+++ Source/BBall/Weapon_Hitscan.cpp
@@ -6,7 +6,28 @@
 
 void AWeapon_Hitscan::Fire()
 {
+	if (Role == ROLE_Authority)
+	{
+		Super::Fire();
 
+		UCameraComponent* PlayerCamera = HoldingPawn->GetFirstPersonCameraComponent();
+
+		float ConeHalfAngle = FMath::DegreesToRadians(HitscanConfig.HipFireSpread * 0.5f);
+
+		FVector StartTrace = PlayerCamera->GetComponentLocation();
+		FVector ShootDir = FVector(0.f, 0.f, 0.f);
+		//FVector ShootDir = FVector(PlayerCamera->GetForwardVector, ConeHalfAngle, ConeHalfAngle);
+		FVector EndTrace = (ShootDir * HitscanConfig.HitscanRange) + StartTrace;
+
+		FHitResult Hit = TraceHit(StartTrace, EndTrace);
+
+		bool bHitEnemy = ProcessHit(Hit);
+
+		if (bHitEnemy)
+		{
+			//show hitmarker
+		}
+	}
 }
 
 FHitResult AWeapon_Hitscan::TraceHit(FVector StartTrace, FVector EndTrace)
